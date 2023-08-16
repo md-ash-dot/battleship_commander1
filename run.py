cc2177caa3f3d172992ffbc30f1b803372c9fff1
md-ash-dot <>
2023-08-16 17:26:04 +0000

    Add __init__ function with parameters for the game to run

--- run.py
+++ run.py
@@ -5,3 +5,14 @@ class Board:
     The Board class, sets the board size, the number of ships,
     player's name, board type.
     """
+    
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type 
+        self.guesses = []
+        self.ships = []
+
+    
