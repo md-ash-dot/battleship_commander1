03bd94e12799b1fc2c078b4b9fd2eeaecc9399af
md-ash-dot <>
2023-08-16 20:53:19 +0000

    Add function to add ships on board

--- run.py
+++ run.py
@@ -15,19 +15,27 @@ class Board:
         self.board = [["." for _ in range(size)] for _ in range(size)]
         self.num_ships = num_ships
         self.name = name
-        self.board_type = board_type 
+        self.board_type = board_type
         self.guesses = []
         self.ships = []
 
     def guess(self, x, y):
         self.guesses.append((x,y))
-        self.board[x][y] = "X"
 
         if (x, y) in self.ships:
             self.board[x][y] = "*"
             return "Hit"
         else:
+            self.board[x][y] = "X"
             return "Miss"
+
+    def add_ship(self, x, y):
+        if len(self.ships) >= self.num_ships:
+            print("Error: you cannot add any more ships!")
+        else:
+            self.ships.append((x,y))
+            if self.board_type == "player":
+                self.board[x][y] = "@"
 
 
 
