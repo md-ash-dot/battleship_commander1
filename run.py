de4cd7c74ce08b15b507e6be67bdd33748d3a89c
md-ash-dot <>
2023-08-16 17:12:01 +0000

    Test

--- run.py
+++ run.py
@@ -1,2 +1,14 @@
-hello = "Hello World!"
-print(hello)
+scores = {"computer": 0, "player": class Board:
+    """
+    Board class, sets the board size, the number of ships, 
+    the player's name and the board type.
+    """
+
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+   
+
+print("Test")
